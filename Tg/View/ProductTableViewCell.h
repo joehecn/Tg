49205e92a368e0f54d32dd63019a9e2922883bc7
何苗 <leanbrown@live.cn>
2018-03-20 02:34:18 +0800

    2-4

---
+++ Tg/View/ProductTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ProductTableViewCell.h
+//  Tg
+//
+//  Created by 何苗 on 2018/3/20.
+//  Copyright © 2018年 joehe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductTableViewCell : UITableViewCell
+
+@end
---
+++ Tg/View/ProductTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ProductTableViewCell.m
+//  Tg
+//
+//  Created by 何苗 on 2018/3/20.
+//  Copyright © 2018年 joehe. All rights reserved.
+//
+
+#import "ProductTableViewCell.h"
+
+@implementation ProductTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
